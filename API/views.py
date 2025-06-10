e923b5ed6dd702ba53658373e57bc4460033cfbc
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-06-10 09:18:27 +0300

    configuration et définitionde vue dans views.py

--- API/views.py
+++ API/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.parsers import MultiPartParser, FormParser
+from rest_framework import status
+from PIL import Image
+import io
+from django.http import HttpResponse
+from rembg import remove
 
-# Create your views here.
+class Index(APIView):
+
+    parser_classes = [MultiPartParser, FormParser]
+
+    def post(self, request, format=None):
+        if 'image' not in request.FILES:
+            return Response({"error": "No image file provided."}, status=status.HTTP_400_BAD_REQUEST)
+        
+        image_file = request.FILES['image']
+
+        try:
+            input_image = Image.open(image_file)
+            output_image = remove(input_image)
+
+            # Sauvegrader dans un buffer
+            buffered = io.BytesIO()
+            output_image.save(buffered, content_type="image/png")
+
+        except Exception as e :
+            return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR) 
