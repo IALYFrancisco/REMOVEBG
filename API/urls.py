5b469fe7322589d9bf898000db62418563e58eda
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-06-11 09:34:00 +0300

    modification de l'endpoint

--- API/urls.py
+++ API/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import Index
 
 urlpatterns = [
-    path('removebg/', Index.as_view(), name="RemoveBackground")
+    path('removebg', Index.as_view(), name="RemoveBackground")
 ]
