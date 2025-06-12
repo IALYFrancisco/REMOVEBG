5066cfe779036c9b3ed7118bd4af3b3e67da0cac
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-06-12 07:42:58 +0300

    définition de route pour V1

--- API/urls.py
+++ API/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import V2
+from .views import V1, V2
 
 urlpatterns = [
-    path('v5', V2.as_view(), name="RemoveBackground")
+    path('v1', V1.as_view(), name="RemoveBackgroundV1"),
+    path('v2', V2.as_view(), name="RemoveBackgroundV2")
 ]
