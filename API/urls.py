7a6e7497882bdf7ffcb2ac4f1e1cb0ad4d259d18
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-06-12 07:08:53 +0300

    définition de dernière code en v5

--- API/urls.py
+++ API/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import Index
 
 urlpatterns = [
-    path('removebg', Index.as_view(), name="RemoveBackground")
+    path('v5', Index.as_view(), name="RemoveBackground")
 ]
